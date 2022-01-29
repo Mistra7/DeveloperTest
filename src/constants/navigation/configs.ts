f4f7fdec2ab9d6056050955df70a988d8fcea91a
Mistra7 <mihajlo.mihajlo.dzever@gmail.com>
2022-01-29 15:09:59 +0100

    Icon support

--- src/constants/navigation/configs.ts
+++ src/constants/navigation/configs.ts
@@ -2,7 +2,11 @@ import {BottomTabNavigationOptions} from '@react-navigation/bottom-tabs';
 import {StackNavigationOptions} from '@react-navigation/stack';
 
 export const bottomTabNavigatorConfig: BottomTabNavigationOptions = {
-  headerShown: false,
+  headerShown: true,
+  headerTitleAlign: 'center',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
 };
 
 export const rootNavigatorConfig: StackNavigationOptions = {
